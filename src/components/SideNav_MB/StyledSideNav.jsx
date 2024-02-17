a04cead2ff4598990c835281eb84d3c52b5c1a0c
Deaf Web Admin <linux.user@techie.com>
2024-02-17 10:36:08 -0600

    mas correcciones en SideNav e implementacion de iconos

--- src/components/SideNav_MB/StyledSideNav.jsx
+++ src/components/SideNav_MB/StyledSideNav.jsx
@@ -1,4 +1,29 @@
 import styled from 'styled-components'
+import homesvg from './Icons/home.svg'
+import orderssvg from './Icons/list-check.svg'
+import productssvg from './Icons/bookmark.svg'
+import categoriessvg from './Icons/folder.svg'
+import useraltsvg from './Icons/users-alt.svg'
+import reportssvg from './Icons/signal-alt-1.svg'
+import couponssvg from './Icons/star.svg'
+import appssvg from './Icons/apps.svg'
+import kbsvg from './Icons/info.svg'
+import updatesvg from './Icons/download.svg'
+import usersvg from './Icons/user.svg'
+import settingssvg from './Icons/settings.svg'
+
+const home = homesvg
+const orders = orderssvg
+const products = productssvg
+const categories = categoriessvg
+const useralt = useraltsvg
+const reports = reportssvg
+const coupon = couponssvg
+const apps = appssvg
+const kb = kbsvg
+const update = updatesvg
+const user = usersvg
+const settings = settingssvg
 
 const DivNav = styled.div`
 width:310px;
@@ -25,6 +50,8 @@ border:0px;
 display:flex;
 flex-direction:row;
 margin:2px;
+gap:6px;
+padding:3px;
 
 align-items:${(props) =>
 props.disabled === 'disabled'
@@ -44,12 +71,17 @@ height:${(props) =>
 color:${(props) =>
     props.disabled === 'disabled'
     ? 'var(--general80)'
+    : props.modo === 'night'
+    ? 'var(--general60)'
     : 'var(--general100)'};
-  
+img{
+  width:20px;
+  height:20px;
+}  
   &:hover{
   color:${(props) =>
     props.modo === 'night'
-    ? 'white'
+    ? 'Black'
     : 'var(--general30)'};
   background:${(props) =>
     props.modo === 'night'
@@ -64,4 +96,4 @@ background-image: url('./Icons/home.svg');
 background-repeat: no-repeat;
 background-size: 20px;
 `
-export { DivNav, DivNavItem, DivIcon }
+export { DivNav, DivNavItem, DivIcon, home, orders, products, categories, useralt, reports, coupon, apps, kb, update, user, settings }
--- src/components/SideNav_MB/Viewer.jsx
+++ src/components/SideNav_MB/Viewer.jsx
@@ -1,23 +1,23 @@
-import { StyledNav, StyledNavItem, StyledIcon } from './SideNav'
-
+import { StyledNav, StyledNavItem } from './SideNav'
+import { home, products, orders, categories, useralt, reports, coupon, apps, kb, update, user, settings } from './StyledSideNav'
 const SideNavViewer = () => {
   return (
     <>
-    <StyledNav >
-      <StyledNavItem ><StyledIcon></StyledIcon>Dashboard</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Orders</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Products</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Categories</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Customers</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Reports</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Coupons</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Aplications</StyledNavItem>
-      <StyledNavItem disabled='disabled'><StyledIcon></StyledIcon>Other Information</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Knowledge Base</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Products Update</StyledNavItem>
-      <StyledNavItem disabled='disabled'><StyledIcon></StyledIcon>Settings</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Personal Settings</StyledNavItem>
-      <StyledNavItem ><StyledIcon></StyledIcon>Global Settings</StyledNavItem>
+    <StyledNav modo='nights'>
+      <StyledNavItem modo='nights'><img src={home}></img>Dashboard</StyledNavItem>
+      <StyledNavItem ><img src={orders}></img>Orders</StyledNavItem>
+      <StyledNavItem ><img src={products}></img>Products</StyledNavItem>
+      <StyledNavItem ><img src={categories}></img>Categories</StyledNavItem>
+      <StyledNavItem ><img src={useralt}></img>Customers</StyledNavItem>
+      <StyledNavItem ><img src={reports}></img>Reports</StyledNavItem>
+      <StyledNavItem ><img src={coupon}></img>Coupons</StyledNavItem>
+      <StyledNavItem ><img src={apps}></img>Aplications</StyledNavItem>
+      <StyledNavItem disabled='disabled'>Other Information</StyledNavItem>
+      <StyledNavItem ><img src={kb}></img>Knowledge Base</StyledNavItem>
+      <StyledNavItem ><img src={update}></img>Products Update</StyledNavItem>
+      <StyledNavItem disabled='disabled'>Settings</StyledNavItem>
+      <StyledNavItem ><img src={user}></img>Personal Settings</StyledNavItem>
+      <StyledNavItem ><img src={settings}></img>Global Settings</StyledNavItem>
     </StyledNav>
     </>
   )
