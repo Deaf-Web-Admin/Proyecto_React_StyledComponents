93eec746b127ac0a16ccf908c2f79f3660351b26
Deaf Web Admin <linux.user@techie.com>
2024-03-05 01:46:28 -0600

    adicion de claves unicas / Fin del Proyecto

--- src/components/Listado_MB/ListadoViewer.jsx
+++ src/components/Listado_MB/ListadoViewer.jsx
@@ -7,91 +7,103 @@ const ListadoViewer = () => {
     tamano: 'big',
     texto: 'Name',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '1'
   },
   {
     tipo: 'Encabezado',
     tamano: '',
     texto: 'Registered',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '2'
   },
   {
     tipo: 'Encabezado',
     tamano: '',
     texto: 'Avg. Order',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '3'
   },
   {
     tipo: 'Encabezado',
     tamano: '',
     texto: 'Spent',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '4'
   },
   {
     tipo: '',
     tamano: 'big',
     texto: 'John Anderson',
     contenido: 'user',
-    imgplaceholder: placeholder
+    imgplaceholder: placeholder,
+    clave: '5'
   },
   {
     tipo: '',
     tamano: '',
     texto: '24.05.2020',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '6'
   },
   {
     tipo: '',
     tamano: '',
     texto: '$75.50',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '7'
   },
   {
     tipo: '',
     tamano: '',
     texto: '225.90$',
     contenido: '',
-    imgplaceholder: ''
+    imgplaceholder: '',
+    clave: '8'
   },
   {
     tipo: '',
     tamano: 'big',
     texto: 'Andrew Spencer',
     contenido: 'user',
-    imgplaceholder: placeholder
+    imgplaceholder: placeholder,
+    clave: '9'
   },
   {
     tipo: '',
     tamano: '',
     texto: '24.05.2020',
     contenido: '',
-    placeholder: ''
+    placeholder: '',
+    clave: '10'
   },
   {
     tipo: '',
     tamano: '',
     texto: '$54.60',
     contenido: '',
-    placeholder: ''
+    placeholder: '',
+    clave: '11'
   },
   {
     tipo: '',
     tamano: '',
     texto: '320.15$',
     contenido: '',
-    placeholder: ''
+    placeholder: '',
+    clave: '12'
   }
   ]
   return (
     <>
       <StyledList>
       {Listados.map((Listado) => (
-          <StyledItem tipo={Listado.tipo} contenido={Listado.contenido} tamano={Listado.tamano} key={Listado.texto}>
+          <StyledItem tipo={Listado.tipo} contenido={Listado.contenido} tamano={Listado.tamano} key={Listado.clave}>
             <img src={Listado.imgplaceholder}></img>{Listado.texto}</StyledItem>
       ))}
       </StyledList>
